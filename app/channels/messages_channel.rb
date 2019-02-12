883f86c2a51880ff867bfa50f90032acf56f6f0a
github username <github email address>
2019-02-12 12:26:44 -0500

    this should be working

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -1,5 +1,7 @@
 class MessagesChannel < ApplicationCable::Channel
+
   def subscribed
+    # byebug
     @chatroom = ChatRoom.find(params[:chat_room_id])
     stream_for @chatroom
   end
--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -8,12 +8,12 @@ class Api::V1::MessagesController < ApplicationController
   def create
     @message = Message.create(text: params[:text], user_id: params[:user_id], chat_room_id: params[:chat_room_id])
     render json: @message
-    # @chatroom = ChatRoom.find(params[:chat_room_id])
-    # @serialized_data = ActiveModelSerializers::Adapter::Json.new(
-    #   MessageSerializer.new(@message)
-    # ).serializable_hash
-    # MessagesChannel.broadcast_to @chatroom, @message
-    # head :ok
+    @chatroom = ChatRoom.find(params[:chat_room_id])
+    @serialized_data = ActiveModelSerializers::Adapter::Json.new(
+      MessageSerializer.new(@message)
+    ).serializable_hash
+    MessagesChannel.broadcast_to @chatroom, @message
+    head :ok
   end
 
 end
